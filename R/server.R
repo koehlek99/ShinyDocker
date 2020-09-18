7f3e4e335f0db7d3287b6158308fd4419f30eb72
koehlek99 <koehlek99@zedat.fu-berlin.de>
2020-09-18 12:34:39 +0200

    fixed file paths

--- R/server.R
+++ R/server.R
@@ -1,9 +1,10 @@
 
-setwd("C:/")
-
-             
 server <- function(input, output, session){
   
+  if(.Platform$OS.type == "windows") sep <- "\\"
+  else sep <- "/"
+  
+  setwd(sep)
   
   shinyDirChoose(input,"dir", session=session,roots=c(wd=getwd()))
    
@@ -20,49 +21,49 @@ server <- function(input, output, session){
     }
   })
   
-
   
   observeEvent(input$creport, {
     observe({
       if(input$dtype=="MaxQuant output folder"){
         path.old <- parseDirPath(roots=c(wd=getwd()), input$dir)
-        path.new <- paste0(tempfile("report", tempdir(), "\\"))
+        path.new <- paste0(tempfile("report", tempdir(), sep))
         dir.create(path.new)
         files <- dir(path.old)
-        file.copy(paste0(path.old, "\\", files), paste0(path.new, "\\", files))
+        file.copy(paste0(path.old, sep, files), paste0(path.new, sep, files))
         
-        #if(input$yaml) yaml.obj <- read.delim2(input$yaml$datapath)
-        #else {}
-        yaml.obj <- NULL
+        if(input$yaml) yaml.obj <- yaml.load_file(input$yaml$datapath)
+        else  yaml.obj <- list()
+       
         createReport(txt_folder = path.new, mztab_file = NULL, yaml_obj = yaml.obj)
         
         getPage<-function() {
-          return(includeHTML(paste0(path.new, "\\", list.files(path = path.new, pattern = "report.*html"))))
+          return(includeHTML(paste0(path.new, list.files(path = path.new, pattern = "report.*html"))))
         }
         output$htmlpage<-renderUI({getPage()})
         
         output$pdfdownload <- downloadHandler(
           filename = "PTXQC_Report.pdf", 
           content = function(file){
-            file.copy(paste0(path.new, "\\", list.files(path = path.new, pattern = "report.*pdf")), file)
+            file.copy(paste0(path.new, list.files(path = path.new, pattern = "report.*pdf")), file)
           }
         )
       } else {
         
         mztab_file <- input$file$datapath
-        #if(input$yaml) yaml.obj <- read.delim2(input$yaml$datapath)
-        #else {}
-        yaml.obj <- NULL
+        
+        if(input$yaml) yaml.obj <- yaml.load_file(input$yaml$datapath)
+        else yaml.obj <- list()
+        
         createReport(txt_folder = NULL, mztab_file = mztab_file, yaml_obj = yaml.obj)
         
         output$pdfdownload <- downloadHandler(
           filename = "Report.pdf",
           content = function(file){
-            file.copy(paste0(dirname(input$file$datapath), "\\", list.files(path = dirname(input$file$datapath), pattern = c("report.*pdf"))), file)
+            file.copy(paste0(dirname(input$file$datapath), sep, list.files(path = dirname(input$file$datapath), pattern = c("report.*pdf"))), file)
           }
         )
         getPage<-function() {
-          return(includeHTML(paste0(dirname(input$file$datapath), "\\", list.files(path = dirname(input$file$datapath), pattern = "report.*html"))))
+          return(includeHTML(paste0(dirname(input$file$datapath), sep, list.files(path = dirname(input$file$datapath), pattern = "report.*html"))))
         }
         output$htmlpage<-renderUI({getPage()})
       }
@@ -72,9 +73,6 @@ server <- function(input, output, session){
 }
 
 shinyApp(ui,server)
-
-# mztab_file <- "C:/Users/Krissi/Desktop/bachelorarbeit/test ptxcq/example.mzTab"
-# createReport(txt_folder = NULL, mztab_file = mztab_file)
 
 
 ##output$dir.txt directory path MaxQuant output 
--- R/ui.R
+++ R/ui.R
@@ -1,4 +1,4 @@
-list.of.packages <- c("shiny", "PTXQC", "dplyr", "shinycssloaders", "shinyFiles", "shinythemes", "shinyjs")
+list.of.packages <- c("shiny", "PTXQC", "dplyr", "shinycssloaders", "shinyFiles", "shinythemes", "shinyjs", "magrittr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages, dependencies = TRUE)
 
@@ -6,6 +6,7 @@ if(length(new.packages)) install.packages(new.packages, dependencies = TRUE)
 library(shiny)
 library(PTXQC)
 library(dplyr)
+library(magrittr)
 library(shinycssloaders)
 library(shinyFiles)
 library(shinythemes)
